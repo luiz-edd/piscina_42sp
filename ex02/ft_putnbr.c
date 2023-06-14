bf89db8d3db0631c40aea303fac96db7478185c0
Luiz Eduardo D <leduard2@student.42sp.org.br>
2023-06-14 09:15:57 -0300

    last commit

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leduard2 <leduard2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 11:35:29 by leduard2          #+#    #+#             */
+/*   Updated: 2023/06/13 11:42:37 by leduard2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_int(int nb)
+{
+	if (nb > 9)
+	{
+		ft_print_int(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else
+	{
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= -1;
+		ft_print_int(nb);
+	}
+	else
+	{
+		ft_print_int(nb);
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_putnbr(-2147483648);
+// }
+// // revisado
